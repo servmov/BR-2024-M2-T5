99627da87c7f77d28a1dba6e03e209ce8bb96fc2
João Kleber <joaokleber968@outlook.com>
2023-09-14 17:23:06 -0300

    TAREFA: BIRD, LARGE_CACTUS

--- dino_runner/components/obstacles/cactus.py
+++ dino_runner/components/obstacles/cactus.py
@@ -4,8 +4,10 @@ from dino_runner.components.obstacles.obstacle import Obstacle
 
 
 class Cactus(Obstacle):
-    def __init__(self, image):
+    def __init__(self, images):
+        index = random.randint(0, 1)
+        image = images[index]
         self.type = random.randint(0, 2)
         super().__init__(image, self.type)
 
-        self.rect.y = 325
+        self.rect.y = 325 if index == 0 else 300
--- dino_runner/components/obstacles/obstacle.py
+++ dino_runner/components/obstacles/obstacle.py
@@ -1,3 +1,4 @@
+import random
 import pygame
 from pygame.sprite import Sprite
 
